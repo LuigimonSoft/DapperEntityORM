6ba82a23e621c0fadfb29971e024ad91a3d9f79a
Luigimon <almadejaguar@hotmail.com>
2023-02-28 16:56:47 -0500

    Add the entity attributes

---
+++ DapperEntityORM/Attributes/ColumnAttribute.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace DapperEntityORM.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class ColumnAttribute: Attribute
+    {
+        private int _maxLength = -1;
+        private int _minLength = -1;
+
+        /// <summary>
+        ///  Max length of the column
+        /// </summary>
+        /// <value>Default value -1 (no limit)</value>
+        public int MaxLength{
+            get { return _maxLength; }
+            set { _maxLength = value; }
+        }
+
+        /// <summary>
+        /// Min length of the column
+        /// </summary>
+        /// <value>Default value -1 (No minimum)</value>
+        public int MinLength{
+            get { return _minLength; }
+            set { _minLength = value; }
+        }
+
+        /// <summary>
+        /// Column name of the property
+        /// </summary>
+        public string ColumnName { get; set; }    
+
+        /// <summary>
+        /// Regular expression pattern to validate the column
+        /// </summary>
+        public string RegExPattern { get; set; }
+
+        /// <summary>
+        /// Allow empty string
+        /// </summary>
+        public bool AllowEmpty { set; get; }
+
+        /// <summary>
+        /// Null custom error message
+        /// </summary>
+        public string ErrorNullMessage { get; set; }
+
+        /// <summary>
+        /// Empty custom error message
+        /// </summary>
+        public string ErrorEmptyMessage { get; set; }
+
+        /// <summary>
+        /// Maximum custom error message
+        /// </summary>
+        public string ErrorMaximunMessage { get; set; }
+
+        /// <summary>
+        /// Minimum custom error message
+        /// </summary>
+        public string ErrorminimumMessage { get; set; }
+
+        /// <summary>
+        /// RegEx custom error message
+        /// </summary>
+        public string ErrorRegExMessage { get; set; }
+
+        /// <summary>
+        /// Property to ignore column
+        /// </summary>
+        public bool Ignore { get; set; }
+
+        /// <summary>
+        /// Property to ignore column in an select query
+        /// </summary>
+        public bool IgnoreInSelect { get; set; }
+        /// <summary>
+        /// Property to ignore column in an insert query
+        /// </summary>
+        public bool IgnoreInInsert { get; set; }
+        /// <summary>
+        /// Property to ignore column in an update query
+        /// </summary>
+        public bool IgnoreInUpdate { get; set; }
+        /// <summary>
+        /// Property to ignore column in an delete query
+        /// </summary>
+        public bool IgnoreInDelete { get; set; }
+
+        public ColumnAttribute(){}
+
+        public ColumnAttribute(string columnName)
+        {
+            ColumnName = columnName;
+        }
+
+    }
+}
---
+++ DapperEntityORM/Attributes/KeyAttribute.cs
@@ -0,0 +1,46 @@
+﻿
+namespace DapperEntityORM.Attributes
+{
+    /// <summary>
+    /// Attribute to define the primary key of a table
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class KeyAttribute : Attribute
+    {
+
+        /// <summary>
+        /// The name of the primary key
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The type of the primary key
+        /// </summary>
+        public Type Type { get; set; }
+
+        /// <summary>
+        /// The length of the primary key
+        /// </summary>
+        public int Length { get; set; }
+
+        /// <summary>
+        /// The default value of the primary key
+        /// </summary>
+        public object DefaultValue { get; set; }
+
+        /// <summary>
+        //// The constructor of the KeyAttribute
+        /// </summary>
+        /// <param name="name">The name of the primary key</param>
+        /// <param name="type">The type of the primary key</param>
+        /// <param name="length">The length of the primary key</param>
+        /// <param name="defaultValue">The default value of the primary key</param>
+        public KeyAttribute(string name, Type type, int length, object defaultValue)
+        {
+            Name = name;
+            Type = type;
+            Length = length;
+            DefaultValue = defaultValue;
+        }
+    }
+}
