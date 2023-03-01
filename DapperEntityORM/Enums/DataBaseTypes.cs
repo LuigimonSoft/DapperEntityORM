4390fe7ae4b2a875996390c85125b36a2543c6f3
Luigimon <almadejaguar@hotmail.com>
2023-03-01 16:26:55 -0500

    Add enum databases

---
+++ DapperEntityORM/Enums/DataBaseTypes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperEntityORM.Enums
+{
+    /// <summary>
+    /// Database types
+    /// </summary>
+    public enum DataBaseTypes
+    {
+        SQLServer,
+        PostgreSQL,
+        SQLite,
+        MySQL
+    }
+}
