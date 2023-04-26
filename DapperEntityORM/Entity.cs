95525460ddd02c56248e5fe94058cef844550aa2
Luigimon <almadejaguar@hotmail.com>
2023-04-26 16:30:33 -0400

    Add async methosd to a class Entity

--- DapperEntityORM/Entity.cs
+++ DapperEntityORM/Entity.cs
@@ -331,6 +331,43 @@ namespace DapperEntityORM
             return Load(primaryKeyValue);
         }
 
+        public async Task<bool> LoadAsync()
+        {
+            bool mapColum;
+            string idName = _columnNameResolver.ResolveKeyColumnName(GetKeyProperty(this.GetType()), _database.Encapsulation, out mapColum);
+            object? idValue = GetKeyProperty(GetType()).GetValue(this);
+            var EntityLoaded = await Select(_database).Where($"{idName}=@{idName.Replace("[", "").Replace("]", "")}", new List<object>() { idValue }).SingleAsync();
+            if (EntityLoaded != null)
+            {
+                foreach (var property in EntityLoaded.GetType().GetProperties())
+                {
+                    var value = property.GetValue(EntityLoaded);
+                    property.SetValue(this, value);
+                }
+                _columnsModified.Clear();
+                isNew = false;
+                return true;
+            }
+
+            return false;
+        }
+        public async Task<bool> LoadAsync(object primaryKeyValue)
+        {
+            GetKeyProperty(GetType()).SetValue(this, primaryKeyValue);
+            if (_database == null)
+                throw new Exception("The entity must have a database");
+            if (string.IsNullOrEmpty(_tableName))
+                init();
+            return await LoadAsync();
+        }
+        public async Task<bool> LoadAsync(object primaryKeyValue, DataBase database)
+        {
+            this._database = database;
+            if (string.IsNullOrEmpty(_tableName))
+                init();
+            return await LoadAsync(primaryKeyValue);
+        }
+
         public bool Save()
         {
             List<string> Errors = new List<string>();
@@ -369,6 +406,57 @@ namespace DapperEntityORM
             }
             else
                 return Insert(out Errors, throwErrors);
+        }
+        public async Task<bool> SaveAsync()
+        {
+            List<string> Errors = new List<string>();
+            return Save(out Errors, true);
+        }
+        public async Task<(bool success, List<string> errors)> SaveAsync(bool throwErrors = false)
+        {
+            List<string> Errors = new List<string>();
+            PropertyInfo? propertyKey = (PropertyInfo?)GetKeyProperty(GetType());
+            if (propertyKey == null)
+            {
+                setError(Errors, "The entity must have a key property", throwErrors);
+                return (false, Errors);
+            }
+
+            object? idValue = propertyKey.GetValue(this);
+            if (!isNew && idValue != null)
+            {
+                if (idValue.GetType() == typeof(int))
+                {
+                    if ((int)idValue == 0)
+                    {
+                        (bool success, List<string> errors) res = await InsertAsync(throwErrors);
+                        return (res.success, res.errors);
+                    }
+                }
+                else if (idValue.GetType() == typeof(string))
+                {
+                    if (string.IsNullOrEmpty((string)idValue))
+                    {
+                        (bool success, List<string> errors) res = await InsertAsync(throwErrors);
+                        return (res.success, res.errors);
+                    }
+                }
+                else if (idValue.GetType() == typeof(Guid))
+                {
+                    if ((Guid)idValue == Guid.Empty)
+                    {
+                        (bool success, List<string> errors) res = await InsertAsync(throwErrors);
+                        return (res.success, res.errors);
+                    }
+                }
+                (bool success, List<string> errors) res2 = await UpdateAsync(throwErrors);
+                return (res2.success, res2.errors);
+            }
+            else
+            {
+                (bool success, List<string> errors) res = await InsertAsync(throwErrors);
+                return (res.success, res.errors);
+            }
         }
 
         public bool Update()
@@ -479,6 +567,116 @@ namespace DapperEntityORM
             }
           
             return false;
+        }
+        public async Task<bool> UpdateAsync()
+        {
+            List<string> Errors = new List<string>();
+            var result = await UpdateAsync(true);
+            return result.success;
+        }
+        public async Task<(bool success, List<string> errors)> UpdateAsync(bool throwErrors = false)
+        {
+            List<string> Errors = new List<string>();
+
+            PropertyInfo? propertyKey = (PropertyInfo?)GetKeyProperty(GetType());
+            if (propertyKey == null)
+                setError(Errors, "The entity must have a key property", throwErrors);
+            if (_database == null)
+                setError(Errors, "The entity must have a database", throwErrors);
+
+            IsValid(out Errors, throwErrors);
+
+            object? idValue = propertyKey.GetValue(this);
+            if (idValue == null)
+                setError(Errors, "The key value is null", throwErrors);
+            else if (idValue.GetType() == typeof(int))
+                if ((int)idValue == 0)
+                    setError(Errors, "The key value is 0", throwErrors);
+                else if (idValue.GetType() == typeof(string))
+                    if (string.IsNullOrEmpty((string)idValue))
+                        setError(Errors, "The key value is empty", throwErrors);
+                    else if (idValue.GetType() == typeof(Guid))
+                        if ((Guid)idValue == Guid.Empty)
+                            setError(Errors, "The key value is empty", throwErrors);
+
+            if (Errors.Count > 0)
+                return (false,Errors);
+
+            if (_columnsModified.Count > 0)
+            {
+                int resId = -1;
+                string sql = $"UPDATE {_tableName} SET ";
+                string setSQL = string.Empty;
+                foreach (var property in _columnsModified)
+                {
+                    var columnattr = property.GetCustomAttributes(true).SingleOrDefault(attr => attr.GetType() == typeof(ColumnAttribute)) as ColumnAttribute;
+                    string columnName = getColumnName(property, out bool isMapColumn);
+                    if (isMapColumn && !columnattr.IgnoreInUpdate)
+                    {
+                        if (setSQL != string.Empty)
+                            setSQL += ",";
+                        setSQL += $" {columnName}=@{property.Name}";
+                    }
+                }
+
+                string idName = _columnNameResolver.ResolveKeyColumnName(propertyKey, _database.Encapsulation, out bool mapColum);
+                string whereSQL = $" WHERE {idName}=@{idName.Replace("[", "").Replace("]", "")}";
+                using (IDbConnection Conexion = _database.Connection)
+                {
+                    resId = await Conexion.ExecuteAsync(sql + setSQL + whereSQL, this);
+                    _columnsModified.Clear();
+                    isNew = false;
+                    if (resId > 0)
+                    {
+                        foreach (PropertyInfo propertyrelation in GetPropertiesRelationUpdatable(this.GetType()))
+                        {
+
+                            var propertyValue = propertyrelation.GetValue(this, null) as dynamic;
+                            if (propertyValue != null)
+                            {
+
+                                if (propertyrelation.PropertyType.IsGenericType && propertyrelation.PropertyType.GetGenericTypeDefinition() == typeof(List<>))
+                                {
+                                    for (int x = 0; x < propertyValue.Count; x++)
+                                    {
+                                        propertyValue[x].GetType().GetMethod("SetDataBase").Invoke(propertyValue[x], new object[] { _database });
+                                        object[] parameters = new object[] { new List<string>(), throwErrors };
+                                        (bool success, List<string> errors)  res = await ((MethodInfo[])propertyValue[x].GetType().GetMethods()).Where(m => m.Name == "SaveAsync" && m.ToString() == "Boolean SaveAsync(Boolean)").FirstOrDefault()?.Invoke(propertyValue[x], parameters);
+                                        addError(Errors, res.errors);
+                                    }
+
+                                }
+                                else if (propertyrelation.PropertyType.IsGenericType && propertyrelation.PropertyType.GetGenericTypeDefinition() == typeof(Dictionary<,>))
+                                {
+                                    Dictionary<int, string> pv = new Dictionary<int, string>();
+                                    foreach (var pkey in propertyValue.Keys)
+                                    {
+                                        propertyValue[pkey].GetType().GetMethod("SetDataBase").Invoke(propertyValue[pkey], new object[] { _database });
+                                        object[] parameters = new object[] { new List<string>(), throwErrors };
+                                        (bool success, List<string> errors) res = await ((MethodInfo[])propertyValue[pkey].GetType().GetMethods()).Where(m => m.Name == "SaveAsync" && m.ToString() == "Boolean SaveAsync(Boolean)").FirstOrDefault()?.Invoke(propertyValue[pkey], parameters);
+                                        addError(Errors, res.errors);
+                                    }
+                                }
+                                else
+                                {
+                                    propertyValue.GetType().GetMethod("SetDataBase").Invoke(propertyValue, new object[] { _database });
+                                    object[] parameters = new object[] { new List<string>(), throwErrors };
+                                    (bool success, List<string> errors) res = await ((MethodInfo[])propertyValue.GetType().GetMethods()).Where(m => m.Name == "SaveAsync" && m.ToString() == "Boolean SaveAsync(Boolean)").FirstOrDefault()?.Invoke(propertyValue, parameters);
+                                    addError(Errors, res.errors);
+                                }
+
+
+                            }
+                        }
+                        return (true, Errors);
+                    }
+                    else
+                        return (false, Errors);
+                }
+
+            }
+
+            return (false, Errors);
         }
 
         public bool Delete()
@@ -652,6 +850,142 @@ namespace DapperEntityORM
             }
             else
                 return false;
+        }
+
+        public async Task<bool> InsertAsync(DataBase database = null)
+        {
+            List<string> Errors = new List<string>();
+            return (await InsertAsync(true, database)).success;
+        }
+        public async Task<(bool success, List<string> errors)> InsertAsync(bool throwErrors = false, DataBase database = null)
+        {
+            List<string> Errors = new List<string>();
+            if (database != null)
+            {
+                _database = database;
+                _tableName = getTableName(this);
+                _primaryKeyColumn = getKeyColumnName(this);
+                _columns = getColumnsNames();
+            }
+            if (_database == null)
+                setError(Errors, "The entity must have a database", throwErrors);
+
+            IsValid(out Errors, throwErrors);
+
+            if (Errors.Count > 0)
+                return (false, Errors);
+
+            string InserColumnsSQL = string.Empty;
+            string InsertValuesSQL = string.Empty;
+            string sqlScopeIdentity = string.Empty;
+            bool getIdGen = false;
+            int resId = -1;
+
+            PropertyInfo? propertyKey = (PropertyInfo?)GetKeyProperty(GetType());
+
+            if (_columnsModified.Count > 0 && isNew)
+            {
+                foreach (var property in _columnsModified)
+                {
+                    var columnattr = property.GetCustomAttributes(true).SingleOrDefault(attr => attr.GetType() == typeof(ColumnAttribute)) as ColumnAttribute;
+                    string columnName = getColumnName(property, out bool isMapColumn);
+                    if (isMapColumn && !columnattr.IgnoreInInsert)
+                    {
+                        if (InserColumnsSQL != string.Empty)
+                        {
+                            InserColumnsSQL += ",";
+                            InsertValuesSQL += ",";
+                        }
+                        InserColumnsSQL += $" {columnName}";
+                        InsertValuesSQL += $" @{property.Name}";
+                    }
+                }
+                if (propertyKey != null)
+                {
+                    KeyAttribute keyattribute = (KeyAttribute)propertyKey.GetCustomAttributes(true).Where(attr => attr.GetType() == typeof(KeyAttribute)).Single();
+                    object? idValue = propertyKey.GetValue(this);
+
+                    if (idValue == null)
+                    {
+                        if (keyattribute.IsIdentity && (propertyKey.PropertyType == typeof(int?) || propertyKey.PropertyType == typeof(Int32?) || propertyKey.PropertyType == typeof(Int16?)))
+                            getIdGen = true;
+                    }
+                    else
+                    {
+                        if (keyattribute.IsIdentity && (propertyKey.PropertyType == typeof(int) || propertyKey.PropertyType == typeof(Int32) || propertyKey.PropertyType == typeof(Int16)) && (int)idValue == 0)
+                            getIdGen = true;
+                    }
+
+                    if (getIdGen)
+                    {
+                        sqlScopeIdentity = "; SELECT SCOPE_IDENTITY() as Id";
+                    }
+                    else
+                    {
+                        string columnName = getColumnName(propertyKey, out bool isMapColumn);
+                        InserColumnsSQL += $", {columnName}";
+                        InsertValuesSQL += $", @{propertyKey.Name}";
+                    }
+
+                }
+                string sql = $"INSERT INTO {_tableName} ({InserColumnsSQL}) VALUES ({InsertValuesSQL}){sqlScopeIdentity}";
+
+                using (IDbConnection Conexion = _database.Connection)
+                {
+                    if (getIdGen)
+                    {
+                        resId = (await Conexion.QueryAsync<int>(sql, this)).Single();
+                        propertyKey.SetValue(this, resId);
+
+                        foreach (PropertyInfo propertyrelation in GetPropertiesRelationInsertable(this.GetType()))
+                        {
+                            var propertyValue = propertyrelation.GetValue(this, null) as dynamic;
+                            RelationAttribute attributerelation = propertyrelation.GetCustomAttributes(true).Where(attr => attr.GetType() == typeof(RelationAttribute)).Single() as RelationAttribute;
+                            string foreignKeyName = attributerelation.ForeignKey;
+                            if (propertyValue != null)
+                            {
+                                if (propertyrelation.PropertyType.IsGenericType && propertyrelation.PropertyType.GetGenericTypeDefinition() == typeof(List<>))
+                                {
+                                    for (int x = 0; x < propertyValue.Count; x++)
+                                    {
+                                        propertyValue[x].GetType().GetProperty(foreignKeyName).SetValue(propertyValue[x], resId);
+                                        {
+                                            propertyValue[x].GetType().GetMethod("SetDataBase").Invoke(propertyValue[x], new object[] { _database });
+                                            object[] parameters = new object[] { new List<string>(), throwErrors };
+                                            (bool success, List<string> errors) res = await ((MethodInfo[])propertyValue[x].GetType().GetMethods()).Where(m => m.Name == "SaveAsync" && m.ToString() == "Boolean SaveAsync(Boolean)").FirstOrDefault()?.Invoke(propertyValue[x], parameters);
+                                            addError(Errors, res.errors);
+                                        }
+                                    }
+                                }
+                                else if (propertyrelation.PropertyType.IsGenericType && propertyrelation.PropertyType.GetGenericTypeDefinition() == typeof(Dictionary<,>))
+                                {
+                                    Dictionary<int, string> pv = new Dictionary<int, string>();
+                                    foreach (var pkey in propertyValue.Keys)
+                                    {
+                                        propertyValue[pkey].GetType().GetMethod("SetDataBase").Invoke(propertyValue[pkey], new object[] { _database });
+                                        object[] parameters = new object[] { new List<string>(), throwErrors };
+                                        (bool success, List<string> errors)  res = await ((MethodInfo[])propertyValue[pkey].GetType().GetMethods()).Where(m => m.Name == "SaveAsync" && m.ToString() == "Boolean SaveAsync(Boolean)").FirstOrDefault()?.Invoke(propertyValue[pkey], parameters);
+                                        addError(Errors, res.errors);
+                                    }
+                                }
+                                else
+                                {
+                                    propertyValue.GetType().GetMethod("SetDataBase").Invoke(propertyValue, new object[] { _database });
+                                    object[] parameters = new object[] { new List<string>(), throwErrors };
+                                    (bool success, List<string> errors)  res = await ((MethodInfo[])propertyValue.GetType().GetMethods()).Where(m => m.Name == "SaveAsync" && m.ToString() == "Boolean SaveAsync(Boolean)").FirstOrDefault()?.Invoke(propertyValue, parameters);
+                                    addError(Errors, res.errors);
+                                }
+                            }
+                        }
+                    }
+                    else
+                        resId = (await Conexion.ExecuteAsync(sql, this));
+                    _columnsModified.Clear();
+                    return (resId > 0, Errors);
+                }
+            }
+            else
+                return (false,Errors);
         }
 
         private void addError(List<string> groupErrors, List<string> errors)
